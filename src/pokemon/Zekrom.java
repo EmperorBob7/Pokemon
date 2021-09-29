b08d4fb72823d66657f634fc219edaa2defc39e7
Siddharth <60718415+Siddharth3141@users.noreply.github.com>
2021-09-29 17:20:49 -0500

    Added Zekrom

---
+++ src/pokemon/Zekrom.java
@@ -0,0 +1,21 @@
+package pokemon;
+
+public class Zekrom implements Pokemon{
+
+	public String getName() {
+		return "Zekrom";
+	}
+
+	public String getType1() {
+		return "Dragon";
+	}
+
+	public String getType2() {
+		return "Electric";
+	}
+
+	public int getIndex() {
+		return 644;
+	}
+
+}
